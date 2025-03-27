0118f0c662afe3838418669d8109f52b8644bd2e
Chollangi Sri Ram <145741104+ram9711@users.noreply.github.com>
2025-03-27 12:33:48 +0530

    Update HelloController.java

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -7,6 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class HelloController {
+    
+    @GetMapping
+    public String home() {
+        return "Welcome to the Spring Boot App!";
+    }
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, World!";
